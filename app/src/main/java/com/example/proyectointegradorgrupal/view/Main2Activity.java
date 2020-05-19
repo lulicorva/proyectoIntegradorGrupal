a1242b2f0f7ba4bc4bb4f46621906b41eaa8df74
Lucila <luli.c-@hotmail.com>
2020-05-19 18:10:05 -0300

    Toast del Search

--- app/src/main/java/com/example/proyectointegradorgrupal/view/Main2Activity.java
+++ app/src/main/java/com/example/proyectointegradorgrupal/view/Main2Activity.java
@@ -1,10 +1,14 @@
 package com.example.proyectointegradorgrupal.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.proyectointegradorgrupal.R;
 
@@ -25,8 +29,21 @@ public class Main2Activity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menubuscador,menu);
-        return super.onCreateOptionsMenu(menu);
+        final MenuItem searchItem = menu.findItem(R.id.toolbarBuscador);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
 
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                Toast.makeText(Main2Activity.this,newText,Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+        return true;
     }
 }
